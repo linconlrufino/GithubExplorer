9fff4fa6f1c2ba03e6b9a4a123f643a1537c4e68
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-05-23 22:00:41 -0300

    Estilizando Dashboard - 2

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -7,11 +7,12 @@ export const Title = styled.h1`
   color: #3a3a3a;
   max-width: 450px;
   line-height: 56px;
+
   margin-top: 80px;
 `;
 
 export const Form = styled.form`
-  margin-top: 4px;
+  margin-top: 40px;
   max-width: 700px;
 
   display: flex;
@@ -22,6 +23,7 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
 
     &::placeholder {
       color: #a8a8b3;
@@ -40,6 +42,58 @@ export const Form = styled.form`
 
     &:hover {
       background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
     }
   }
 `;
