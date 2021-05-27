be343f75480daea61d29756ff0c59dbbdaf58bfe
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-05-26 23:00:28 -0300

    Conectando a API  - 1

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,44 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  function handleAddRepository() {}
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title> Explore repositórios no Github. </Title>
 
       <Form action="dsd">
-        <input placeholder="Digite o nome do repositório" />
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit"> Pesquisar </button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/62862403?v=4"
-            alt="Linconl"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms! 🚀</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/62862403?v=4"
-            alt="Linconl"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms! 🚀</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
         <a href="teste">
           <img
             src="https://avatars.githubusercontent.com/u/62862403?v=4"
